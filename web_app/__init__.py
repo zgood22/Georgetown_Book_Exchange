cd5e3c2799425215be98bf06119173db2907e2fa
jlm456 <jlm456@georgetown.edu>
2023-11-28 13:02:09 -0500

    Flask and Google Sheets Operational

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,15 +1,26 @@
 # this is the "web_app/__init__.py" file...
 
+# this is the "web_app/__init__.py" file...
+
+import os
+
 from flask import Flask
 
-from web_app.routes import register_routes
-
-#from web_app.routes.book_routes import book_routes
+from web_app.routes.home_routes import home_routes
+#from web_app.routes.unemployment_routes import unemployment_routes
+from web_app.routes.book_routes import book_routes
 #from web_app.routes.weather_routes import weather_routes
+
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret") # set this to something else on production!!!
+
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(register_routes)
+    app.config["SECRET_KEY"] = SECRET_KEY
+
+    app.register_blueprint(home_routes)
+    app.register_blueprint(book_routes)
+    #app.register_blueprint(unemployment_routes)
     #app.register_blueprint(book_routes)
     #app.register_blueprint(weather_routes)
     return app
