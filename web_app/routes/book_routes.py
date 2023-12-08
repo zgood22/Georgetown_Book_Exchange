c45ad6b218070a5bd8a5a7ade168ab86b72cb1a4
jlm456 <jlm456@georgetown.edu>
2023-12-08 16:47:52 -0500

    book form linked to listing search

--- web_app/routes/book_routes.py
+++ web_app/routes/book_routes.py
@@ -1,27 +1,7 @@
 from flask import Blueprint, request, render_template
+from app.pull_book import get_book_details
 
 book_routes = Blueprint("book_routes", __name__)
-
-
-@book_routes.route("/book")
-def book_form():
-    print("Book Form...")
-    return render_template("book_form.html")
-
-@book_routes.route('/submit_book', methods=['POST'])
-def submit_book():
-    title = request.form.get('title')
-    author = request.form.get('author')
-    description = request.form.get('description')
-    new_book = {
-        'title': title,
-        'author': author,
-        'description': description
-    }
-    #books.append(new_book)
-    print(new_book)
-    return new_book
-    
 
 
 # Assuming you have an existing Blueprint named 'book_routes'
@@ -30,3 +10,7 @@ def submit_book():
 @book_routes.route('/new-listing')
 def new_listing():
     return render_template('book_form.html')
+
+@book_routes.route('/listing-search')
+def listing_search():
+    return render_template('listing_search.html')
